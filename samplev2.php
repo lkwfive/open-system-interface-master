343092ab52c9b87118323a97cd7c2363011d6686
unknown <likunwu@sailvan.com>
2019-10-16 13:56:10 +0800

    add v2

---
+++ samplev2.php
@@ -0,0 +1,17 @@
+<?php
+require './src/OpenInterfaceV2.php';
+use OpenSystemInterface\OpenInterfaceV2;
+
+$config = [
+    'user_code' => 'J3ADP_XXXXX_XXXXX_XXXXX_XXXXX',             //开发者编号
+    'app_token' => '5722ed62e91eb3c5fc8bfa30bfb3701d',          //开发者token
+    'server' => 'http://baidu.com',    //入口URL
+];
+
+$obj = new OpenInterfaceV2($config);
+// 方法 版本
+$r = $obj->get('order.test', 'V1.1');
+$r = json_decode($r, true);
+if($r['status']){
+    var_dump($r['data']);
+}
---
+++ src/OpenInterfaceV2.php
@@ -0,0 +1,88 @@
+<?php
+namespace OpenSystemInterface;
+/**
+* 
+*/
+class OpenInterfaceV2
+{
+    public $info=[];//header info
+
+    function __construct($config)
+    {
+        $this->user_code = $config['user_code'];
+        $this->app_token = $config['app_token'];
+        $this->server = $config['server'];
+    }
+
+    protected function curlGet($url, $timeout=30)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $content = curl_exec($ch);
+        $this->info = curl_getinfo($ch);
+        curl_close($ch);
+        return $content;
+    }
+
+    protected function curlPost($url, $data, $timeout=30)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        $content = curl_exec($ch);
+        $this->info = curl_getinfo($ch);
+        curl_close($ch);
+        return $content;
+    }
+
+    protected function createSign($paramArr) { 
+        $str = ""; 
+        ksort($paramArr); 
+        foreach ($paramArr as $key => $val) { 
+           if ($key !='' && $val !='') { 
+               $str .=  $key.$val; 
+           } 
+        }
+        $sign = strtoupper(md5($str.$this->app_token));
+        return  $sign; 
+    }
+
+    /**
+     * 内网开放系统不支持post
+     */
+    // public function post($method, $params, $local=false)
+    // {
+    //     $config = [
+    //         'v'         => '1.0',
+    //         'username'  => $this->user,
+    //         'method'    => $method,
+    //     ];
+    //     $params = array_merge($config, $params);
+    //     $sign = $this->createSign($params);
+    //     $params['sign'] = $sign;
+    //     $url = $local ? $this->url_local : $this->url;
+    //     $url = $url.http_build_query($params);
+    //     return $this->curlPost($url, $params);
+    // }
+
+    public function get($method, $v='1.0', $params=[])
+    {
+        $config = [
+            'method' => $method,
+            'v' => $v,
+            'format' => 'json',
+            'app_key' => $this->user_code,
+            'protocol' => 'param3',
+        ];
+        $params = array_merge($config, $params);
+        $sign = $this->createSign($params);
+        $params['sign'] = $sign;
+        $url = $this->server.http_build_query($params);
+        return $this->curlGet($url);
+    }
+}
